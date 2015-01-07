de1266a347c25d2b6a42b71ccbd63702d7d37463
Zack Fisch <zack@zfisch.com>
2015-01-07 15:10:23 -0500

    Creating log-in session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,20 @@
 class SessionsController < ApplicationController
 
 	def new
+
 	end
 
 	def create
+		user = User.find_by(email: params[:email])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect_to root_url
+		else
+			flash.now[:alert] = "Invalid email or password!"
+			render "new"
 	end
 
 	def destroy
+
 	end
 end
