05036ea64c47cd1a11c8204d01e3cac5b1752f82
Zack Fisch <zack@zfisch.com>
2015-01-07 17:57:12 -0500

    Session create/destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,13 +8,16 @@ class SessionsController < ApplicationController
 		user = User.find_by(email: params[:email])
 		if user && user.authenticate(params[:password])
 			session[:user_id] = user.id
+			debug(params)
 			redirect_to root_url
 		else
 			flash.now[:alert] = "Invalid email or password!"
-			render "new"
+			render :new
+		end
 	end
 
 	def destroy
-
+		session[:user_id] = nil
+		redirect_to root_url
 	end
 end
