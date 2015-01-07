18e9f317b132de24d8b84f844a60ab7520da4ea8
Zack Fisch <zack@zfisch.com>
2015-01-06 20:36:41 -0500

    Adding child model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
+	has_many :children
+	
+	validates :password, presence: true
+	validates :password_confirmation, presence: true
+	validates :email, presence: true
+
 	has_secure_password
 end
