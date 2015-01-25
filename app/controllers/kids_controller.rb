2df6ac2250eb8ea58dbe66df4c0ada91b800ff25
Zack Fisch <zack@zfisch.com>
2015-01-25 14:47:39 -0500

    Creating form for new child creation

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -1,3 +1,13 @@
 class KidsController < ApplicationController
 
+	def new
+		@user = User.find(params[:user_id])
+		@kid = @user.kids.new
+	end
+
+	def create
+		@user = User.find(params[:user_id])
+		@kid = @user.kids.new
+	end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,6 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
-		@kids = @user.kids
 	end
 
 	def edit
