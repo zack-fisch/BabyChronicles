6b746e9a86c541292c30f7f321438a1a4fbb9081
Zack Fisch <zack@zfisch.com>
2015-01-25 15:41:50 -0500

    Adding validations to Kid model

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -7,7 +7,18 @@ class KidsController < ApplicationController
 
 	def create
 		@user = User.find(params[:user_id])
-		@kid = @user.kids.new
+		@kid = @user.kids.create(kid_params)
+		if @kid.save 
+			redirect_to @user
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def kid_params
+		params.require(:kid).permit(:parent1, :parent2, :parent1bio, :parent2bio, :bio, :birthday,)
 	end
 
 end
--- app/models/kid.rb
+++ app/models/kid.rb
@@ -1,3 +1,10 @@
 class Kid < ActiveRecord::Base
 	belongs_to :user
+
+	validates :parent1, presence: true
+	validates :parent2, presence: true
+	validates :parent1bio, presence: true
+	validates :parent2bio, presence: true
+	validates :bio, presence: true
+	validates :birthday, presence: true
 end
