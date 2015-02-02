d1ef44714e32798b98224dd9652a4f1a563884a3
Zack Fisch <zack@zfisch.com>
2015-02-02 14:54:35 -0500

    Send user email after user create

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,8 @@
 class UserMailer < ApplicationMailer
+
+	def welcome_email(user)
+		@user = user
+		@url = 'http://babychronices.herokuapp.com'
+		mail(to: @user.email, subject: 'Thanks for signing up for BabyChronicles!')
+	end
 end
